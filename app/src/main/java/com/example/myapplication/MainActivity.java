8ee75299cab717ca4fccb659d3e3dc9f35c61ead
Caisman <caiussuciu@gmail.com>
2020-04-21 21:43:11 +0300

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,96 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG= MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleLogin = findViewById(R.id.title_login);
+        TextView titleRegister = findViewById(R.id.title_register);
+
+        String textLogin = titleLogin.getText().toString();
+        String textRegister = titleRegister.getText().toString();
+
+        Log.d(TAG, textLogin+" "+textRegister);
+
+        ImageView imageView= findViewById(R.id.image);
+        final Button nextButton = findViewById(R.id.next);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"I click next button"+ nextButton.getText().toString());
+            }
+        });
+
+        final EditText emailField = findViewById(R.id.email_field); //R fisier regenerat from build
+
+        emailField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d(TAG,"beforeTextChanged "+ s.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(TAG,"onTextChanged "+ s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d(TAG,"afterTextChanged "+ emailField.getText().toString());
+            }
+        });
+
+        CheckBox checkBox = findViewById(R.id.acceptTerms);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG,"onCheckedChanged "+ isChecked);
+            }
+        });
+
+        Switch notificationSwitch = findViewById(R.id.notifications);
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "onCheckedChanged "+ isChecked);
+            }
+        });
+
+        RatingBar ratingBar= findViewById(R.id.rating);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.d(TAG,"onRatingChanged"+ fromUser);
+            }
+        });
+
+        ((RadioGroup) findViewById(R.id.radio_group)).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Log.d(TAG, "onRatingChanged "+radioButton.getText().toString());
+            }
+        });
     }
 }
